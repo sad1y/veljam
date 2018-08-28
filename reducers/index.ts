ed4d3d79a828ee6b303649096c7b3d4b55eaaeb2
zoth <max.itcn@gmail.com>
2018-08-28 16:25:57 +0300

    fix import pathJpsTrace

--- reducers/index.ts
+++ reducers/index.ts
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import boxEditor, { boxEditorInitState } from '../pages/area-editor/reducers';
-import jpsTrace, { jpsTraceInitState } from '../pages/path-builder-jps-trace/reducers';
+import pathJpsTrace, { jpsTraceInitState } from '../pages/path-builder-jps-trace/reducers';
 
 export const rootState: State.IRoot = {
   boxEditor: boxEditorInitState,
   pathJpsTrace: jpsTraceInitState
 };
 
-export default combineReducers({ boxEditor, jpsTrace });
+export default combineReducers({ boxEditor, pathJpsTrace });
