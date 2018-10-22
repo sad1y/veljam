e4da0d340d3a026adaeab8d46f261d7683b52d8a
zoth <max.itcn@gmail.com>
2018-10-22 07:33:22 +0300

    unsorted queue

--- alghoritms/queues.ts
+++ alghoritms/queues.ts
@@ -1,3 +1,8 @@
+export interface PathFindigingNode {
+  iteration: number;
+  cost: number;
+}
+
 export const bucketPriorityQueue = (buckets: number, arraySize: number, division: number) => {};
 
 export class BucketPriorityQueue {
@@ -12,23 +17,54 @@ export class BucketPriorityQueue {
   }
 }
 
-export class UnsortedPriorityQueue<T extends { iteration: number; finalCost: number }> {
+export class UnsortedPriorityQueue<T extends PathFindigingNode> {
   nextFreeNode = 0;
   iteration = 0;
-  nodeArray: T[] = [];
+  nodeArray: T[] = null;
   identical: boolean;
+
+  constructor(size: number) {
+    this.nodeArray = new Array(size);
+  }
 
   push = (node: T) => {
     if (this.iteration !== node.iteration) {
       this.nextFreeNode = 0;
       this.identical = true;
       this.iteration = node.iteration;
-    } else if (this.identical) {
-      this.identical = node.finalCost == this.nodeArray[0].finalCost;
-    } else {
-      this.identical = this.nextFreeNode === 0;
     }
 
     this.nodeArray[this.nextFreeNode++] = node;
+  };
+
+  remove = (node: T) => {
+    for (let i = 0; i < this.nextFreeNode; i++) {
+      if (this.nodeArray[i] === node) {
+        // put last node where this one was
+        this.nodeArray[i] = this.nodeArray[--this.nextFreeNode];
+        return;
+      }
+    }
+  };
+
+  pop = () => {
+    if (!this.nodeArray.length) return null;
+
+    let cost = this.nodeArray[0].cost;
+    let index = 0;
+
+    for (let i = 1; i < this.nodeArray.length; i++) {
+      const node = this.nodeArray[i];
+
+      if (node.cost < cost) {
+        cost = node.cost;
+        index = i;
+      }
+    }
+
+    // remove element
+    this.nodeArray[index] = this.nodeArray[--this.nextFreeNode];
+
+    return this.nodeArray[index];
   };
 }
